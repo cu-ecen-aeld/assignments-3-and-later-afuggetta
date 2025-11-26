0a190647e6f34b1fa980b9dcdc6c076dab1028c0
Andrea Fuggetta <afuggetta@gmail.com>
2025-11-26 13:07:30 -0500

    more testing with other app

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -32,11 +32,17 @@
 struct aesd_dev
 {
     struct cdev cdev;                     /* Char device structure      */
-    struct aesd_circular_buffer buffer;   /* Our circular buffer        */
-    struct mutex lock;                    /* Mutex for read/write       */
-    char   *partial_buf;                  /* Accumulated partial command*/
-    size_t  partial_size;                 /* Size of partial_buf        */
+    struct aesd_circular_buffer circ_buf;
+    const char waiting_bfr[WAITING_BUFFER_SIZE];
+    uint8_t waiting_bfr_offset;
+    struct mutex lock;
 };
 
+int aesd_open(struct inode *inode, struct file *filp);
+int aesd_release(struct inode *inode, struct file *filp);
+ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,loff_t *f_pos);
+ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,loff_t *f_pos);
+int aesd_init_module(void);
+void aesd_cleanup_module(void);
 
 #endif /* AESD_CHAR_DRIVER_AESDCHAR_H_ */
